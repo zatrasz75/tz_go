8034e23c82c196e5a6821cb9a96325df0e5f0bed
Zatrasz <zatrasz@ya.ru>
2024-04-10 18:43:05 +0300

    migrations sql-migrate

--- migrations/20240410171419-up.sql
+++ migrations/20240410171419-up.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS cars;
-DROP TABLE IF EXISTS people;
 
+-- +migrate Up
 CREATE TABLE IF NOT EXISTS people (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     patronymic VARCHAR(255)
-);
+    );
 
 CREATE TABLE IF NOT EXISTS cars (
     id SERIAL PRIMARY KEY,
@@ -16,4 +15,8 @@ CREATE TABLE IF NOT EXISTS cars (
     year INTEGER,
     owner_id INTEGER,
     FOREIGN KEY (owner_id) REFERENCES people(id)
-);
+    );
+
+-- +migrate Down
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS people;
